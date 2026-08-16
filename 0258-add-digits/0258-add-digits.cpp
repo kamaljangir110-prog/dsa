af9f5101cd779084c1b09731863bd446e9d2a27c
kamaljangir110-prog <kamaljangir110@gmail.com>
2026-08-17 00:09:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.96%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10){
+
+          int ans=0;
+           int rem;
+            while(num>0){
+                rem = num%10;
+               num = num/10;
+               ans = ans+rem;
+            }   
+            num =ans;
+        }
+         return num;
+
+    }
+        //  return ans;
+
+};
