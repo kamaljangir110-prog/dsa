0818761769917fda0d6b6b466d8574cb7efe33fe
kamaljangir110-prog <kamaljangir110@gmail.com>
2026-08-17 00:06:12 +0530

    Time: 3 ms (43.98%), Space: 8.7 MB (36.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return 0;
+        }
+        int rem,num=x;
+        int ans = 0;
+        while(num){
+            rem = num%10;
+            num =num/10;
+            if(ans>(INT_MAX/10)||ans<(INT_MIN/10)){
+                return 0;
+            }
+            ans = ans*10+rem;
+
+        };
+        if(ans==x)
+         return 1;
+        else
+        return 0;
+    }
+};
